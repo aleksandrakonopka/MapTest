7b8ea3803f10e79e6f9e8c2803d14fe517ddfc15
aleksandrakonopka <aleksandrakonopka424@gmail.com>
2019-03-16 14:44:25 +0100

    save01

--- MapTest/ViewController.swift
+++ MapTest/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate{
+    
+    @IBOutlet var myMap: MKMapView!
+    private let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let annotationWZIM = MKPointAnnotation()
+        let annotationJezyki = MKPointAnnotation()
+        let annotations = [annotationWZIM,annotationJezyki]
+        annotationWZIM.coordinate = CLLocationCoordinate2DMake(52.161992, 21.046240)
+        annotationJezyki.coordinate = CLLocationCoordinate2DMake(52.157267, 21.044686)
+        annotationWZIM.title = "Budynek 34"
+        annotationWZIM.subtitle = "Wydział Zastosowań Informatyki i Matematyki"
+        annotationJezyki.title = "Budynek Językowy"
+        annotationJezyki.subtitle = "Język Angielski"
+        self.myMap.addAnnotations(annotations)
+        
+        self.myMap.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.distanceFilter = kCLDistanceFilterNone
+        self.myMap.mapType = .standard
+        //self.myMap.mapType = .satellite
+        //self.myMap.mapType = .hybrid
+        locationManager.startUpdatingLocation()
+        self.myMap.showsUserLocation = true
     }
-
-
 }
 
