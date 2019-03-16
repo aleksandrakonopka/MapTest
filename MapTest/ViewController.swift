0ed565fe6b31afd1c35782b0de6b4ed0274eea22
aleksandrakonopka <aleksandrakonopka424@gmail.com>
2019-03-16 16:58:16 +0100

    save002

--- MapTest/ViewController.swift
+++ MapTest/ViewController.swift
@@ -13,32 +13,35 @@ import CoreLocation
 class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate{
     
     @IBOutlet var myMap: MKMapView!
+    @IBOutlet var textField: UITextField!
     private let locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let annotationWZIM = MKPointAnnotation()
-        let annotationJezyki = MKPointAnnotation()
-        let annotations = [annotationWZIM,annotationJezyki]
-        annotationWZIM.coordinate = CLLocationCoordinate2DMake(52.161992, 21.046240)
-        annotationJezyki.coordinate = CLLocationCoordinate2DMake(52.157267, 21.044686)
-        annotationWZIM.title = "Budynek 34"
-        annotationWZIM.subtitle = "Wydział Zastosowań Informatyki i Matematyki"
-        annotationJezyki.title = "Budynek Językowy"
-        annotationJezyki.subtitle = "Język Angielski"
-        self.myMap.addAnnotations(annotations)
         
         self.myMap.delegate = self
         locationManager.requestAlwaysAuthorization()
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
         locationManager.distanceFilter = kCLDistanceFilterNone
-        self.myMap.mapType = .standard
-        //self.myMap.mapType = .satellite
-        //self.myMap.mapType = .hybrid
         locationManager.startUpdatingLocation()
         self.myMap.showsUserLocation = true
+    }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if textField.text != ""
+        {
+            let geoCoder = CLGeocoder()
+            geoCoder.geocodeAddressString(textField.text!) { (places, error) in
+                if let error = error {
+                    print(error.localizedDescription)
+                }
+                if let places = places
+                {
+                    print(places.first!.location!.coordinate.latitude)
+                    print(places.first!.location!.coordinate.longitude)
+                }
+            }
+        }
     }
 }
 
